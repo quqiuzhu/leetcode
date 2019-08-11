e2aee9492561a31ab13a2a232fb2c535db77494f
屈秋竹 <quqiuzhu@qq.com>
2019-08-11 09:56:13 +0800

    84.largest-rectangle-in-histogram.go update stack method

--- 84.largest-rectangle-in-histogram.go
+++ 84.largest-rectangle-in-histogram.go
@@ -5,7 +5,34 @@ package main
  *
  * [84] Largest Rectangle in Histogram
  */
-func largestRectangleArea(heights []int) int {
+
+func largestRectangleAreaStackMethod(heights []int) int {
+	heights = append(heights, 0)
+	var max, area, i, top, h, w int
+	var stack []int
+	for i < len(heights) {
+		top = len(stack) - 1
+		if len(stack) == 0 || heights[i] > heights[stack[top]] {
+			stack = append(stack, i)
+			i++
+			continue
+		}
+		h = heights[stack[top]]
+		stack = stack[:top]
+		w = i - 1 - 0 + 1
+		if len(stack) > 0 {
+			top = len(stack) - 1
+			w = i - 1 - stack[top]
+		}
+		area = h * w
+		if area > max {
+			max = area
+		}
+	}
+	return max
+}
+
+func largestRectangleAreaOrdinaryMethod(heights []int) int {
 	var max, area, i, j, h int
 	for i = 0; i < len(heights); i++ {
 		if i < len(heights)-1 && heights[i] < heights[i+1] {
@@ -23,4 +50,8 @@ func largestRectangleArea(heights []int) int {
 		}
 	}
 	return max
+}
+
+func largestRectangleArea(heights []int) int {
+	return largestRectangleAreaStackMethod(heights)
 }
